e789b9fb9feaeb5b5276642370b516e4f923f18b
sinkinben <sinkinben@outlook.com>
2021-10-17 15:30:01 +0800

    feat: add some debug info

--- common.h
+++ common.h
@@ -18,6 +18,19 @@ buffer_t *new_buffer_input()
     return input_buffer;
 }
 
+void print_help()
+{
+    printf("Meta Commands: \n");
+    printf("0) .help: print help information,\n");
+    printf("1) .exit: exit tinydb and save data on disk.\n");
+    printf("2) .constants: print some constants for debugging.\n");
+    printf("3) .btree: print b+tree of current table.\n");
+    printf("\nSQL Statement: \n");
+    printf("1) select (only one `select` keyword :-), and it will print all rows on table)\n");
+    printf("2) insert [id:int] [name:str] [email:str]\n");
+    printf("3) update [id:int] [name:str] [email:str]\n");
+}
+
 void print_prompt()
 {
     printf("tinydb > ");
@@ -43,7 +56,7 @@ void close_input_buffer(buffer_t *input)
     free(input);
 }
 
-// 执行 meta 命令: .exit, .constants, .btree
+// 执行 meta 命令: .exit, .constants, .btree, .help
 meta_command_result_t do_meta_command(buffer_t *input, table_t *table)
 {
     if (strcmp(input->buffer, ".exit") == 0)
@@ -63,12 +76,16 @@ meta_command_result_t do_meta_command(buffer_t *input, table_t *table)
         print_btree(table->pager, table->root_page_num, 0);
         return META_COMMAND_SUCCESS;
     }
+    else if (strcmp(input->buffer, ".help") == 0)
+    {
+        print_help();
+        return META_COMMAND_SUCCESS;
+    }
     else
     {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
 }
-
 
 // 真正调用底层 API 执行 SQL, 相当于 sqlite 的 Core
 execute_result_t execute_statement(statement_t *statement, table_t *table)
--- debug.h
+++ debug.h
@@ -1,13 +1,37 @@
+#include "btree.h"
 #ifndef DEBUG_H
 #define DEBUG_H
 void print_constants()
 {
-    printf("ROW_SIZE: %d\n", ROW_SIZE);
-    printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
-    printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
-    printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
-    printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
-    printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+    printf("table row size: %d\n", ROW_SIZE);
+    printf("  - id:       %u\n", ID_SIZE);
+    printf("  - username: %u\n", USERNAME_SIZE);
+    printf("  - email:    %u\n", EMAIL_SIZE);
+
+    printf("common node header size: %d\n", COMMON_NODE_HEADER_SIZE);
+    printf("  - node_type: %d\n", NODE_TYPE_SIZE);
+    printf("  - is_root:   %d\n", IS_ROOT_SIZE);
+    printf("  - parent:    %d\n", PARENT_POINTER_SIZE);
+
+    printf("leaf node:\n");
+    printf("  + header size:      %d\n", LEAF_NODE_HEADER_SIZE);
+    printf("    - num_cells: %d\n", LEAF_NODE_NUM_CELLS_SIZE);
+    printf("    - next_leaf: %d\n", LEAF_NODE_NEXT_LEAF_SIZE);
+    printf("  + cell size:        %d\n", LEAF_NODE_CELL_SIZE);
+    printf("    - key size: %d\n", LEAF_NODE_KEY_SIZE);
+    printf("    - row size: %d\n", LEAF_NODE_VALUE_SIZE);
+    printf("  + spaces for cells: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+    printf("  + max cells:        %d\n", LEAF_NODE_MAX_CELLS);
+
+    printf("internal node:\n");
+    printf("  + header size:      %d\n", INTERNAL_NODE_HEADER_SIZE);
+    printf("    - num_cells:   %d\n", INTERNAL_NODE_NUM_KEYS_SIZE);
+    printf("    - right_child: %d\n", INTERNAL_NODE_RIGHT_CHILD_SIZE);
+    printf("  + cell size:        %d\n", INTERNAL_NODE_CELL_SIZE);
+    printf("    - child: %d\n", INTERNAL_NODE_CHILD_SIZE);
+    printf("    - key:   %d\n", INTERNAL_NODE_KEY_SIZE);
+    printf("  + spaces for cells: %d\n", INTERNAL_NODE_SPACE_FOR_CELLS);
+    printf("  + max cells:        %d\n", INTERNAL_NODE_MAX_CELLS);
 }
 
 void indent(int32_t level)
